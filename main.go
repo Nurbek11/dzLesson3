e74cf88b99126d960649faa4882850dd38855d1f
Nurbek <nurbeknesipbai000@gmail.com>
2021-06-15 13:41:22 +0600

    sortImport

--- main.go
+++ main.go
@@ -1,36 +1,9 @@
 package main
 
-import (
-	"fmt"
-	dz3Lesson "github.com/Nurbek11/dz3Lesson/fibonacci"
-	dz3Lesson2 "github.com/Nurbek11/dz3Lesson/reverse"
-	dz3Lesson3 "github.com/Nurbek11/dz3Lesson/runeByIndex"
-	dz3Lesson4 "github.com/Nurbek11/dz3Lesson/sortImports"
-	"strconv"
-)
-
 func main() {
+	a := 10
+	p := &a
 
-	var word string = "OneTech"
-	var idx int = 500
-	fmt.Println("ATOI")
-	fmt.Println(strconv.Atoi("15"))
-
-	fmt.Println("ITOA:")
-	fmt.Println(strconv.Itoa(idx))
-
-	fmt.Println("REVERSE:")
-	fmt.Println(dz3Lesson2.Reverse(word))
-
-	fmt.Println("FIBO:")
-	generator := dz3Lesson.Fibonacci()
-	for i := 0; i < 15; i++ {
-		fmt.Print(generator(), " ")
-	}
-
-	fmt.Println("RUNE BY INDEX:")
-	fmt.Println(dz3Lesson3.RuneByIndex(&word, &idx))
-
-	//SORT IMPORTS
-	dz3Lesson4.Sort("main.go")
+	println(p)
+	println(*p)
 }
--- sortImports/sort.go
+++ sortImports/sort.go
@@ -1,22 +1,71 @@
-package dz3Lesson
+package main
 
 import (
-	"fmt"
-	"go/ast"
-	"go/parser"
-	"go/token"
+	"bufio"
+	"io/ioutil"
+	"log"
+	"os"
+	"sort"
+	"strings"
 )
 
-func Check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
 func Sort(dir string) {
-	fset := token.NewFileSet()
-	file, err := parser.ParseFile(fset, dir, nil, parser.ParseComments)
+
+	f, err := os.Open(dir)
+
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-	ast.SortImports(fset, file)
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	var idx int
+	for index, element := range lines {
+		if element == ")" {
+			idx = index
+		}
+	}
+
+	var imports = lines[3:idx]
+	sort.Strings(imports)
+
+	input, err := ioutil.ReadFile(dir)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	linesToWrite := strings.Split(string(input), "\n")
+
+	var index = 0
+	for i, line := range linesToWrite {
+		//if strings.Contains(line, "]") {
+		//	lines[i] = "LOL"
+		//}
+		if strings.Contains(line, ")") {
+			break
+		}
+		if i > 2 {
+			linesToWrite[i] = imports[index]
+			index++
+		}
+	}
+	output := strings.Join(linesToWrite, "\n")
+	err = ioutil.WriteFile(dir, []byte(output), 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
+
+func main() {
+	Sort("sortImport")
+
 }
